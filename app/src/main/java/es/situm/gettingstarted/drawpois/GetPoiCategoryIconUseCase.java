6a9f507edc75cdf03d10a0f9e0c52751ec09a3e2
CrisBarreiro <cristina.sbarreiro@gmail.com>
2018-09-21 10:48:46 +0200

    Add poiCategory icon

---
+++ app/src/main/java/es/situm/gettingstarted/drawpois/GetPoiCategoryIconUseCase.java
@@ -0,0 +1,36 @@
+package es.situm.gettingstarted.drawpois;
+
+import android.graphics.Bitmap;
+
+import java.util.Random;
+
+import es.situm.sdk.SitumSdk;
+import es.situm.sdk.error.Error;
+import es.situm.sdk.model.cartography.Poi;
+import es.situm.sdk.utils.Handler;
+
+/**
+ * Created by Cristina S. Barreiro on 21/09/2018.
+ */
+public class GetPoiCategoryIconUseCase {
+
+    public interface Callback{
+        void onSuccess(Bitmap bitmap);
+        void onError(String error);
+    }
+
+    public void getUnselectedIcon(Poi poi, final Callback callback) {
+        SitumSdk.communicationManager().fetchPoiCategoryIconNormal(poi.getCategory(), new Handler<Bitmap>() {
+            @Override
+            public void onSuccess(Bitmap bitmap) {
+                callback.onSuccess(bitmap);
+            }
+
+            @Override
+            public void onFailure(Error error) {
+                callback.onError(error.getMessage());
+            }
+        });
+
+    }
+}
