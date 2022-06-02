092e64f103d793d936bd73722840952ee71b5eb7
Rodrigo Lago <rodrigo.lago@situm.com>
2022-06-02 15:35:04 +0200

    Clear examples

--- app/src/main/java/es/situm/gettingstarted/samplelist/SamplesActivity.java
+++ app/src/main/java/es/situm/gettingstarted/samplelist/SamplesActivity.java
@@ -2,6 +2,7 @@ package es.situm.gettingstarted.samplelist;
 
 import android.content.Intent;
 import android.os.Bundle;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -11,7 +12,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import es.situm.gettingstarted.R;
-import es.situm.gettingstarted.buildingevents.BuildingListActivity;
 import es.situm.gettingstarted.drawbuilding.DrawBuildingActivity;
 import es.situm.gettingstarted.drawpois.DrawPoisActivity;
 import es.situm.gettingstarted.drawposition.DrawPositionActivity;
@@ -40,15 +40,13 @@ public class SamplesActivity
         items.add(new Sample("Indoor-Outdoor positioning", new Intent(this, IndoorOutdoorActivity.class)));
         items.add(new Sample("Draw building over the map", es.situm.gettingstarted.common.selectbuilding.SelectBuildingActivity.createIntent(this, DrawBuildingActivity.class)));
         items.add(new Sample("Draw position over the map", new Intent(this, DrawPositionActivity.class)));
+        items.add(new Sample("Animate the position while walking", new Intent(this, es.situm.gettingstarted.animateposition.SelectBuildingActivity.class)));
+        items.add(new Sample("Draw Route between two points over the map", es.situm.gettingstarted.common.selectbuilding.SelectBuildingActivity.createIntent(this, DrawRouteActivity.class)));
+        items.add(new Sample("Instructions while going to a destination", es.situm.gettingstarted.common.selectbuilding.SelectBuildingActivity.createIntent(this, GuideInstructionsActivity.class)));
         items.add(new Sample("Draw POIs over the map", new Intent(this, DrawPoisActivity.class)));
-        items.add(new Sample("Draw Route between two points over the map",es.situm.gettingstarted.common.selectbuilding.SelectBuildingActivity.createIntent(this, DrawRouteActivity.class)));
+        items.add(new Sample("Show if point is inside a geofence", es.situm.gettingstarted.common.selectbuilding.SelectBuildingActivity.createIntent(this, PointInsideGeofenceActivity.class)));
         items.add(new Sample("Draw realtime devices over the map", es.situm.gettingstarted.common.selectbuilding.SelectBuildingActivity.createIntent(this, RealTimeActivity.class)));
         items.add(new Sample("Key-Value POIs Filtering", new Intent(this, ListBuildingsActivity.class)));
-        items.add(new Sample("Show all the events of a building", new Intent(this, BuildingListActivity.class)));
-        items.add(new Sample("Instructions while going to a destination", es.situm.gettingstarted.common.selectbuilding.SelectBuildingActivity.createIntent(this, GuideInstructionsActivity.class)));
-        items.add(new Sample("Animate the position while walking", new Intent(this, es.situm.gettingstarted.animateposition.SelectBuildingActivity.class)));
-        items.add(new Sample("Show if user is inside an event", new Intent(this, es.situm.gettingstarted.userinsideevent.ListBuildingsActivity.class)));
-        items.add(new Sample("Show if point is inside a geofence", es.situm.gettingstarted.common.selectbuilding.SelectBuildingActivity.createIntent(this, PointInsideGeofenceActivity.class)));
         items.add(new Sample("Update location parameters on the fly", es.situm.gettingstarted.common.selectbuilding.SelectBuildingActivity.createIntent(this, UpdateLocationParamsActivity.class)));
         items.add(new Sample("Fetch Resources using CommunicationManager", es.situm.gettingstarted.common.selectbuilding.SelectBuildingActivity.createIntent(this, FetchResourcesActivity.class)));
 
--- app/src/main/java/es/situm/gettingstarted/userinsideevent/README.md
+++
@@ -1,5 +0,0 @@
-## <a name="positionevents"></a> Calculate if the user is inside en event
-In order to determine if the user is inside the trigger area of a `SitumEvent`, you should intersect every new location with the `trigger` area of every event in the building.
-
-You can see how to do this in `UserInsideEventActivity` (Please note that minimun Android SDK version is 2.25.0):
-
